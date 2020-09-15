617abf97ff7a2b3472f35175c4ad9cfe1ab1f1f8
tongji4m3 <46219861+tongji4m3@users.noreply.github.com>
2020-09-15 16:04:32 +0800

    Initial commit

---
+++ src/test/MainTest.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import JDK8.HashMap;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+public class MainTest
+{
+    public static void main(String[] args)
+    {
+        HashSet
+    }
+}
