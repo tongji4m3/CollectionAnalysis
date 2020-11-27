6f43ec26b8600327cb3a444e923df2599947eaa4
tongji4m3 <1254931237@qq.com>
2020-11-27 11:37:17 +0800

    完成HashSet的源码分析

--- src/JDK8/HashSet.java
+++ src/JDK8/HashSet.java
@@ -1,79 +1,33 @@
-package JDK8;
+package java.util;
 
-import InvalidObjectException;
-import *;
-import HashMap;
-
+import java.io.InvalidObjectException;
 import sun.misc.SharedSecrets;
 
 /**
- * This class implements the <tt>Set</tt> interface, backed by a hash table
- * (actually a <tt>HashMap</tt> instance).  It makes no guarantees as to the
- * iteration order of the set; in particular, it does not guarantee that the
- * order will remain constant over time.  This class permits the <tt>null</tt>
- * element.
- *
- * <p>This class offers constant time performance for the basic operations
- * (<tt>add</tt>, <tt>remove</tt>, <tt>contains</tt> and <tt>size</tt>),
- * assuming the hash function disperses the elements properly among the
- * buckets.  Iterating over this set requires time proportional to the sum of
- * the <tt>HashSet</tt> instance's size (the number of elements) plus the
- * "capacity" of the backing <tt>HashMap</tt> instance (the number of
- * buckets).  Thus, it's very important not to set the initial capacity too
- * high (or the load factor too low) if iteration performance is important.
- *
- * <p><strong>Note that this implementation is not synchronized.</strong>
- * If multiple threads access a hash set concurrently, and at least one of
- * the threads modifies the set, it <i>must</i> be synchronized externally.
- * This is typically accomplished by synchronizing on some object that
- * naturally encapsulates the set.
- *
- * If no such object exists, the set should be "wrapped" using the
- * {@link Collections#synchronizedSet Collections.synchronizedSet}
- * method.  This is best done at creation time, to prevent accidental
- * unsynchronized access to the set:<pre>
- *   Set s = Collections.synchronizedSet(new HashSet(...));</pre>
- *
- * <p>The iterators returned by this class's <tt>iterator</tt> method are
- * <i>fail-fast</i>: if the set is modified at any time after the iterator is
- * created, in any way except through the iterator's own <tt>remove</tt>
- * method, the Iterator throws a {@link ConcurrentModificationException}.
- * Thus, in the face of concurrent modification, the iterator fails quickly
- * and cleanly, rather than risking arbitrary, non-deterministic behavior at
- * an undetermined time in the future.
- *
- * <p>Note that the fail-fast behavior of an iterator cannot be guaranteed
- * as it is, generally speaking, impossible to make any hard guarantees in the
- * presence of unsynchronized concurrent modification.  Fail-fast iterators
- * throw <tt>ConcurrentModificationException</tt> on a best-effort basis.
- * Therefore, it would be wrong to write a program that depended on this
- * exception for its correctness: <i>the fail-fast behavior of iterators
- * should be used only to detect bugs.</i>
- *
- * <p>This class is a member of the
- * <a href="{@docRoot}/../technotes/guides/collections/index.html">
- * Java Collections Framework</a>.
- *
- * @param <E> the type of elements maintained by this set
- *
- * @author  Josh Bloch
- * @author  Neal Gafter
- * @see     Collection
- * @see     Set
- * @see     TreeSet
- * @see     HashMap
- * @since   1.2
+    不保证集合顺序,允许null值
+    操作只需要常数时间
+    非线程安全的,可以用:
+    Set s = Collections.synchronizedSet(new HashSet(...))
+    除了迭代器之身的remove()方法,其他会fail-fast,抛出ConcurrentModificationException异常
  */
 
+/*
+非线程安全
+允许null值
+添加值得时候会先获取对象的hashCode方法，如果hashCode方法返回的值一致，则再调用equals方法判断是否一致，如果不一致才add元素。
+和HashMap继承得差不多
+ */
 public class HashSet<E>
         extends AbstractSet<E>
-        implements Set<E>, Cloneable, Serializable
+        implements Set<E>, Cloneable, java.io.Serializable
 {
     static final long serialVersionUID = -5024744406713321676L;
 
+    // 底层使用HashMap来保存HashSet中所有元素。
     private transient HashMap<E,Object> map;
 
     // Dummy value to associate with an Object in the backing Map
+    // 定义一个虚拟的Object对象作为HashMap的value，将此对象定义为static final。
     private static final Object PRESENT = new Object();
 
     /**
@@ -124,19 +78,14 @@ public class HashSet<E>
     }
 
     /**
-     * Constructs a new, empty linked hash set.  (This package private
-     * constructor is only used by LinkedHashSet.) The backing
-     * HashMap instance is a LinkedHashMap with the specified initial
-     * capacity and the specified load factor.
+     * 以指定的initialCapacity和loadFactor构造一个新的空链接哈希集合。
+     * 此构造函数为包访问权限，不对外公开，实际只是是对LinkedHashSet的支持。
      *
-     * @param      initialCapacity   the initial capacity of the hash map
-     * @param      loadFactor        the load factor of the hash map
-     * @param      dummy             ignored (distinguishes this
-     *             constructor from other int, float constructor.)
-     * @throws     IllegalArgumentException if the initial capacity is less
-     *             than zero, or if the load factor is nonpositive
+     * 实际底层会以指定的参数构造一个空LinkedHashMap实例来实现。
+     * @param initialCapacity 初始容量。
+     * @param loadFactor 加载因子。
+     * @param dummy 标记。
      */
-    //dummy只是为了与其他构造器进行区分
     HashSet(int initialCapacity, float loadFactor, boolean dummy) {
         map = new LinkedHashMap<>(initialCapacity, loadFactor);
     }
@@ -148,6 +97,7 @@ public class HashSet<E>
      * @return an Iterator over the elements in this set
      * @see ConcurrentModificationException
      */
+    //底层实际调用底层HashMap的keySet来返回所有的key
     public Iterator<E> iterator() {
         return map.keySet().iterator();
     }
@@ -171,46 +121,47 @@ public class HashSet<E>
     }
 
     /**
-     * Returns <tt>true</tt> if this set contains the specified element.
-     * More formally, returns <tt>true</tt> if and only if this set
-     * contains an element <tt>e</tt> such that
-     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+     * 如果此set包含指定元素，则返回true。
+     * 更确切地讲，当且仅当此set包含一个满足(o==null ? e==null : o.equals(e))
+     * 的e元素时，返回true。
      *
-     * @param o element whose presence in this set is to be tested
-     * @return <tt>true</tt> if this set contains the specified element
+     * 底层实际调用HashMap的containsKey判断是否包含指定key。
+     * @param o 在此set中的存在已得到测试的元素。
+     * @return 如果此set包含指定元素，则返回true。
      */
     public boolean contains(Object o) {
         return map.containsKey(o);
     }
 
     /**
-     * Adds the specified element to this set if it is not already present.
-     * More formally, adds the specified element <tt>e</tt> to this set if
-     * this set contains no element <tt>e2</tt> such that
-     * <tt>(e==null&nbsp;?&nbsp;e2==null&nbsp;:&nbsp;e.equals(e2))</tt>.
-     * If this set already contains the element, the call leaves the set
-     * unchanged and returns <tt>false</tt>.
+     * 如果此set中尚未包含指定元素，则添加指定元素。
+     * 更确切地讲，如果此 set 没有包含满足(e==null ? e2==null : e.equals(e2))
+     * 的元素e2，则向此set 添加指定的元素e。
+     * 如果此set已包含该元素，则该调用不更改set并返回false。
      *
-     * @param e element to be added to this set
-     * @return <tt>true</tt> if this set did not already contain the specified
-     * element
+     * 底层实际将将该元素作为key放入HashMap。
+     * 由于HashMap的put()方法添加key-value对时，当新放入HashMap的Entry中key
+     * 与集合中原有Entry的key相同（hashCode()返回值相等，通过equals比较也返回true），
+     * 新添加的Entry的value会将覆盖原来Entry的value，但key不会有任何改变，
+     * 因此如果向HashSet中添加一个已经存在的元素时，新添加的集合元素将不会被放入HashMap中，
+     * 原来的元素也不会有任何改变，这也就满足了Set中元素不重复的特性。
+     * @param e 将添加到此set中的元素。
+     * @return 如果此set尚未包含指定元素，则返回true。
      */
-    //如果是第一次加入,则返回true
     public boolean add(E e) {
+        //因为put会返回原Value,如果放入相同的,则返回就不为null,则该方法返回false,代表添加不成功,代表已经有了元素
         return map.put(e, PRESENT)==null;
     }
 
     /**
-     * Removes the specified element from this set if it is present.
-     * More formally, removes an element <tt>e</tt> such that
-     * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>,
-     * if this set contains such an element.  Returns <tt>true</tt> if
-     * this set contained the element (or equivalently, if this set
-     * changed as a result of the call).  (This set will not contain the
-     * element once the call returns.)
+     * 如果指定元素存在于此set中，则将其移除。
+     * 更确切地讲，如果此set包含一个满足(o==null ? e==null : o.equals(e))的元素e，
+     * 则将其移除。如果此set已包含该元素，则返回true
+     * （或者：如果此set因调用而发生更改，则返回true）。（一旦调用返回，则此set不再包含该元素）。
      *
-     * @param o object to be removed from this set, if present
-     * @return <tt>true</tt> if the set contained the specified element
+     * 底层实际调用HashMap的remove方法删除指定Entry。
+     * @param o 如果存在于此set中则需要将其移除的对象。
+     * @return 如果set包含指定元素，则返回true。
      */
     public boolean remove(Object o) {
         return map.remove(o)==PRESENT;
@@ -251,8 +202,8 @@ public class HashSet<E>
      *             (int), followed by all of its elements (each an Object) in
      *             no particular order.
      */
-    private void writeObject(ObjectOutputStream s)
-            throws IOException {
+    private void writeObject(java.io.ObjectOutputStream s)
+            throws java.io.IOException {
         // Write out any hidden serialization magic
         s.defaultWriteObject();
 
@@ -272,8 +223,8 @@ public class HashSet<E>
      * Reconstitute the <tt>HashSet</tt> instance from a stream (that is,
      * deserialize it).
      */
-    private void readObject(ObjectInputStream s)
-            throws IOException, ClassNotFoundException {
+    private void readObject(java.io.ObjectInputStream s)
+            throws java.io.IOException, ClassNotFoundException {
         // Read in any hidden serialization magic
         s.defaultReadObject();
 
@@ -339,4 +290,3 @@ public class HashSet<E>
         return new HashMap.KeySpliterator<E,Object>(map, 0, -1, 0, 0);
     }
 }
-
--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,6 +1,7 @@
 package test;
 
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
@@ -8,5 +9,8 @@ public class MainTest
 {
     public static void main(String[] args)
     {
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("aa");
+        hashSet.add("aa");
     }
 }
