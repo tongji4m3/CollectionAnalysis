fef2a69572224a69fa626512cc5105f558494b38
tongji4m3 <1254931237@qq.com>
2020-12-15 21:22:58 +0800

    Update MainTest.java

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -8,13 +8,6 @@ public class MainTest
 {
     public static void main(String[] args)
     {
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            linkedList.add(i);
-        }
-
-        for (Integer integer : linkedList) {
-            System.out.println(integer);
-        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
     }
 }
